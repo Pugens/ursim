a446e94ccd1e9ea907feae405f9555c08dc001ff
Pugens <egbernardi@gmail.com>
2024-03-05 00:19:23 +0100

    Refactor robot_interaction_lesson scripts and add docker launch script
    - Refactored `robot_interaction_lesson.script` to include additional setup commands, reorganized existing commands, and added new functionality for handling wrist and elbow singularity cases.
    - Created a new script `robot_interaction_lesson_file.script` with similar functionality to the refactored `robot_interaction_lesson.script`.
    - Updated `robot_interaction_lesson.txt` to reflect changes in the `.script` files.
    - Added `launch_docker.bat` for launching a docker container with the necessary ports and volumes for the UR simulator.
    - Removed unused `robot_interaction_lesson.urp` file.

---
+++ launch_docker.bat
@@ -0,0 +1,1 @@
+docker run --rm -it -e ROBOT_MODEL=UR16 -p 6080:6080 -p 29999:29999 -p 30001:30001 -p 30004:30004 -p 30002:30002 -p 50002:50002 -v "%cd%\urcaps:/urcaps" -v "%cd%\programs:/ursim/programs" universalrobots/ursim_e-series
